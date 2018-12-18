63a3ef5220b3f76b5c8011dc9f1576ef64c0d8c9
Avril <wongavril@gmail.com>
2018-12-18 21:54:49 +0800

    Checkpoint 10 completed

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -8,7 +8,7 @@
           <span class="ion-play"></span>
           <span class="ion-pause"></span>
           </button>
-        </td>  
+        </td>
         <td>${song.title}</td>
         <td>${song.duration}</td>
       </tr>
@@ -16,6 +16,7 @@
 
     song.element.on('click', event => {
       player.playPause(song);
+      $('button#play-pause').attr('playState', player.playState);
     });
 
     $('#song-list').append(song.element);
