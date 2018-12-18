8980a2b8eb436541d24884af9511494f48218de0
Avril <wongavril@gmail.com>
2018-12-19 01:28:02 +0800

    assignment 11 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -9,11 +9,10 @@
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) { return; }
-
     const nextSong = album.songs[nextSongIndex];
     helper.playPauseAndUpdate(nextSong);
-    $('#time-control .total-time').text(player.prettyTime(nextSong.duration));
+    if (nextSongIndex >= album.songs.length) { return; }
+    $('#time-control .total-time').text(nextSong.duration);
   });
 
   $('button#previous').on('click', function() {
@@ -21,11 +20,10 @@
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex < 1) { return; }
-
     const previousSong = album.songs[previousSongIndex];
     helper.playPauseAndUpdate(previousSong);
-    $('#time-control .total-time').text(player.prettyTime(previousSong.duration));
+    if (previousSongIndex < 1) { return; }
+    $('#time-control .total-time').text(previousSong.duration);
   });
 
   $('#time-control input').on('input', function (event) {
@@ -37,7 +35,7 @@
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
-    $('#time-control .current-time').text(player.prettyTime(currentTime));
+    $('#time-control .current-time').text(currentTime);
     $('#time-control input').val(percent);
   }, 1000);
 
